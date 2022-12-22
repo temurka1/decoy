df17fa3ca2112c1c9e654f0db5f57250b5f271ad
Timur Urazalinov <timur.urazalinov@rubius.com>
2022-12-22 20:02:24 +0700

    Fixed incorrect input into numeric only text boxes

---
+++ source/Decoy.Common/Behaviors/TextBoxAllowedInputBehavior.cs
@@ -0,0 +1,75 @@
+﻿
+namespace Decoy.Common.Behaviors
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Input;
+
+    using Microsoft.Xaml.Behaviors;
+
+    public class TextBoxAllowedInputBehavior : Behavior<TextBox>
+    {
+        #region Dependency Properties
+
+        public static readonly DependencyProperty RegularExpressionProperty =
+             DependencyProperty.Register("RegularExpression", typeof(string), typeof(TextBoxAllowedInputBehavior),
+             new FrameworkPropertyMetadata(".*"));
+
+        #endregion
+
+        #region Properties
+
+        public string RegularExpression
+        {
+            get => (string)GetValue(RegularExpressionProperty);
+            set => SetValue(RegularExpressionProperty, value);
+        }
+
+        #endregion
+
+        #region Methods
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            AssociatedObject.PreviewTextInput += OnPreviewTextInput;
+            DataObject.AddPastingHandler(AssociatedObject, OnPaste);
+        }
+
+        private void OnPaste(object sender, DataObjectPastingEventArgs e)
+        {
+            if (e.DataObject.GetDataPresent(DataFormats.Text))
+            {
+                string text = Convert.ToString(e.DataObject.GetData(DataFormats.Text));
+
+                if (!Regex.IsMatch(text, RegularExpression))
+                {
+                    e.CancelCommand();
+                }
+            }
+            else
+            {
+                e.CancelCommand();
+            }
+        }
+
+        void OnPreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            e.Handled = !Regex.IsMatch(e.Text, RegularExpression);
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            AssociatedObject.PreviewTextInput -= OnPreviewTextInput;
+            DataObject.RemovePastingHandler(AssociatedObject, OnPaste);
+        }
+
+        #endregion
+    }
+}
