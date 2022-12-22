856b573541ea45df3a32d3b8cd2f7ca87ef74c91
Timur Urazalinov <timur.urazalinov@rubius.com>
2022-12-23 02:26:57 +0700

    Some visual adjustments, colors, brushes, alignment

---
+++ source/Decoy.Common/Converters/BooleanToGridHeightConverter.cs
@@ -0,0 +1,26 @@
+﻿namespace Decoy.Common.Converters
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Data;
+
+    public class BooleanToGridHeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is bool flag)
+            {
+                return flag 
+                    ? new GridLength(1, GridUnitType.Star)
+                    : GridLength.Auto;
+            }
+
+            return GridLength.Auto;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
